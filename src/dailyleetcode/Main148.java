fa475786426d58c8c047b1b8d1af3fc2813496bd
DESKTOP-507JULO\manbou <tjddn0197@gmail.com>
2022-02-27 22:25:18 +0900

    greedy

--- src/dailyleetcode/Main148.java
+++ src/dailyleetcode/Main148.java
@@ -1,16 +1,17 @@
 package dailyleetcode;
 
+import java.util.*;
+
 public class Main148 {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
     public class Solution {
 
         public ListNode sortList(ListNode head) {
